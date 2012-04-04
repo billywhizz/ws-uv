82627336ca2f443d9da1c9385f960fb3d0281c70
Andrew Johnston <apjohnsto@gmail.com>
2012-04-04 20:58:32 +0100

    updated sha1 with links to sources

--- sha1.h
+++ sha1.h
@@ -3,4 +3,10 @@
 #include <stdint.h>
 #include <string.h>
 
+/*
+most of the code here was taken from:
+https://code.google.com/p/smallsha1/source/browse/trunk/sha1.cpp?spec=svn4&r=4
+and base64 code was taken from:
+http://stackoverflow.com/questions/342409/how-do-i-base64-encode-decode-in-c
+*/
 void shacalc(const char* src, char* dest);
